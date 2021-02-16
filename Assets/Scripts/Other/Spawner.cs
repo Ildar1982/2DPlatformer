c389031fee43ec8b2d4a0a1c96457f1b13b30734
Ildar1982 <68797569+Ildar1982@users.noreply.github.com>
2021-02-16 05:46:49 +0500

    init

---
+++ Assets/Scripts/Other/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _money;
+    [SerializeField] private Transform _spawnPoint;
+
+    private WaitForSeconds _waitForSeconds = new WaitForSeconds(5f);
+    private float _randomX;
+    private float _randomY;
+
+    private void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+    
+    private IEnumerator Spawn()
+    {
+        while (true)
+        {
+            _randomX = Random.Range(-5, 28);
+            _randomY = Random.Range(-2, 0);
+           _spawnPoint.position = new Vector2 (_randomX, _randomY);
+            Instantiate(_money, _spawnPoint.position,Quaternion.identity);            
+            yield return _waitForSeconds;
+        }
+    }
+}
